4a1c63adf0d7e669bbbbbbfbd007e49d21abbe00
endlessczz <endlessczz@gmail.com>
2025-06-30 18:58:48 -0500

    Revise the main function to run all raw AMF files

--- 01_Data_processing/01_AMF_processing.R
+++ 01_Data_processing/01_AMF_processing.R
@@ -1,36 +1,55 @@
 # Author: Zhaozhe Chen
-# Update date: 2025.6.29
+# Update date: 2025.6.30
 
 # This code conducts QC for AMF dataset
 # Output SM, ET
 # More descriptions here (need update)
 # This is a test code, just to get hourly SM and ET for US-Ne1
 
+
+
+
 # --------- Global ---------
+library(here)
 library(dplyr)
 library(lubridate)
-# Input path to raw AMF dataset
-setwd("D:/OneDrive - UW-Madison/Research/ET Synchrony/Data/")
-# Source functions for AMF processing
-source("./../Codes/Github_backup/01_Data_processing/AMF_processing_functions.R")
 
-# Input path to raw AMF dataset
-Input_path <- "./01_AMF_raw/"
-# Output path for cleaned AMF hourly dataset
-Output_path <- "./"
+# Input path to AMF dataset
+Data_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Data/"
+# Source functions for AMF processing
+source(here("01_Data_processing","AMF_processing_functions.R"))
+# Output path for figures
+Output_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Results/Hourly_AMF_TS/"
+# The raw hourly dataset is divided into 5 files for all sites, so use
+# arrayid to determine which file to process (1-5)
+arrayid <- 2
+# This is a list of required variables for output
+var_ls <- c("SWC", # Soil Water Content, Unit: %
+            "TA", # Air temperature, Unit: degree C
+            "VPD", # Vapor pressure deficit, Unit: hPa
+            "P_F", # Precipitation (to be separated with pressure), Unit: mm
+            "WS", # Wind speed, Unit: ms-1
+            "USTAR", # Friction velocity, Unit: ms-1
+            "NETRAD", # Net radiation, Unit: Wm-2
+            "TS", # Soil temperature, Unit: degree C
+            "LE_F" # Latent heat flux, Unit: Wm-2
+            )
 
 # ---------- Main ---------
 gc()
-# Get site info for PFT at each site
-Site_info <- read.csv(paste0(Input_path,"ameriflux_site_info.csv"))
+# Get site info of each site
+#Site_info <- read.csv(paste0(Data_path,"01_AMF_raw/ameriflux_site_info.csv"))
 
-# List all dataset file names
-file_names <- list.files(Input_path,pattern = "_data_",full.names = TRUE)
+# Get file names for the 5 files
+file_names <- list.files(paste0(Data_path,"01_AMF_raw"),pattern = "_data_",full.names = TRUE)
+# Read in raw AMF dataset
+AMF_raw <- read.csv(file_names[arrayid])
+# For each required variable, QC all sub-variables for them, and get the average of all sub-variables for each variable
+AMF_df <- Var_QC_all(var_ls,AMF_raw)
 
-# Read in the raw file
-# Only test at one site for now
-i <- 2
-AMF_raw <- read.csv(file_names[i])
+
+
+
 # Only keep data for the target site
 AMF_site <- AMF_raw %>%
   filter(site_id == "US-Ne1")
