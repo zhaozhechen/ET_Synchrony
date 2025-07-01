5a91f7d3413da93ec587f11740bd3dd9506436a4
endlessczz <endlessczz@gmail.com>
2025-06-30 19:11:57 -0500

    Aggregate all data to hourly scale

--- 01_Data_processing/01_AMF_processing.R
+++ 01_Data_processing/01_AMF_processing.R
@@ -47,6 +47,14 @@ AMF_raw <- read.csv(file_names[arrayid])
 # For each required variable, QC all sub-variables for them, and get the average of all sub-variables for each variable
 AMF_df <- Var_QC_all(var_ls,AMF_raw)
 
+# Aggregate all data to hourly data (some AMF sites have half-hourly data)
+AMF_df <- AMF_df %>%
+  mutate(Time_hour = floor_date(Time,unit="hour")) %>%
+  group_by(Time_hour,Site_ID) %>%
+  summarise(across(where(is.numeric),~ mean(.x,na.rm=TRUE)),
+            .groups = "drop") %>%
+  rename(Time = Time_hour)
+  
 
 
 
@@ -61,26 +69,7 @@ SM <- SWC/100
 # Get ET Unit: Convert from W/m2 to mm/day
 ET <- Mean_QC_Var("LE_F",AMF_site,Coarse = FALSE) * 3600 * 24/(2.45*10^6)
 ET[ET < 0] <- 0
-# Get Time 
-Time <- AMF_site$date_time
 
-# Get the final df
-AMF_site_cleaned <- data.frame(Time = Time,
-                               SM = SM,
-                               ET = ET)
-# Aggregate to hourly data
-df_hourly <- AMF_site_cleaned %>%
-  mutate(
-    # Convert character to POSIXct
-    Time = ymd_hms(Time,tz="UTC"),
-    # Round down to hour, in case this is half-hourly data
-    Time_hour = floor_date(Time,unit="hour")) %>%
-  group_by(Time_hour) %>%
-  summarise(
-    SM = mean(SM,na.rm=TRUE),
-    ET = mean(ET,na.rm=TRUE),
-    .groups = "drop"
-  )
 
 # Output this data frame
 write.csv(df_hourly,paste0(Output_path,"US-Ne1_hourly_SM_ET_test.csv"))
