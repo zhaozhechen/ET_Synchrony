72ffcb98b18524b2899310360186e0c611c28856
endlessczz <endlessczz@gmail.com>
2025-08-12 13:39:04 -0500

    Apply Savistzky-Golay filter to LAI TS

--- 01_Data_processing/01.5_LAI_processing.R
+++ 01_Data_processing/01.5_LAI_processing.R
@@ -1,12 +1,14 @@
 # Author: Zhaozhe Chen
-# Date: 2025.8.11
+# Date: 2025.8.12
 
 # This code is to smooth LAI using Savitzky-Golay filter
 # And calculate start of season (SOS) and end of season (EOS) for each year, at each sites
 
-
 # --------- Global ----------
 library(signal) # the function sgolayfilt is used to apply Savitzky-Golay smoothing filter
+library(dplyr)
+library(here)
+library(zoo) # the function na.approx is used to replace NA by interpolation
 
 # Input path for LAI data
 LAI_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Data/AMF_LAI/MCD15A3H_Lai_500m_"
@@ -14,29 +16,25 @@ LAI_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Data/AMF_LAI/MCD15A3
 site_info <- read.csv(here("00_data","ameriflux_site_info_update.csv")) %>% select(-X)
 # parameters to use in the SG filter
 # window size
-w <- 13
+windowsize <- 13
 # degree of polynomial
-d <- 4
+degree_p <- 4
 
 # --------- Main ---------
 for(i in 1:nrow(site_info)){
   Site_ID <- site_info$site_id[i]
   # Get LAI data
   LAI_df <- read.csv(paste0(LAI_path,Site_ID,".csv"))
-  # Remove "F" and make it NA
-  
-  # Fill small gaps
-  
-
-  
-  # (optional) fill small gaps so spacing stays regular
-  lai_filled <- na.approx(lai, maxgap = 2, na.rm = FALSE)  # fill ≤2 consecutive gaps (~≤8 days)
-  
-  # Smooth
-  lai_sg <- sgolayfilt(lai_filled, p = p, n = w, m = 0)
-  # Smooth
-  LAI_smooth <- sgolayfilt(LAI_df$Lai,p=d,n=w)
-  
+  LAI_df <- LAI_df %>%
+    mutate(
+      # Remove "F" and make it NA
+      Lai = as.numeric(if_else(Lai == "F",NA,Lai)),
+      # Fill small gaps, fill <=2 consecutive gaps with NA
+      LAI_filled = na.approx(Lai,maxgap=2,na.rm=FALSE),
+      # Apply Savistzky-Golay smoothing filter
+      LAI_smoothed = sgolayfilt(LAI_filled,p = degree_p,n = windowsize)
+    )
+  # Calculate LAI thresholds (50% of max and min LAI)
   
   # Calculate SOS and EOS
   
