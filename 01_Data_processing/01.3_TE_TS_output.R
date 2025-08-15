2c4e8d37337581f569332bba5aa471e36e8e5d4d
endlessczz <endlessczz@gmail.com>
2025-08-15 17:49:33 -0500

    Rerun TE_TS_output using updated soil hydraulic traits in the site_info_update file

--- 01_Data_processing/01.3_TE_TS_output.R
+++ 01_Data_processing/01.3_TE_TS_output.R
@@ -1,5 +1,5 @@
 # Author: Zhaozhe Chen
-# Update Date: 2025.8.4
+# Update Date: 2025.8.15
 
 # This code uses cleaned AMF hourly data as input,
 # and outputs final time series of target variables for TE implementation for all sites
@@ -24,35 +24,40 @@ source(here("01_Data_processing","AMF_processing_functions.R"))
 # Set parallel session
 plan(multisession,workers = availableCores()-1)
 
-# There are 5 cleaned AMF files, arrayid determines which file to process
-arrayid <- 5
-
 # ------ Main ----------
-# Read in the cleaned AMF hourly data
-AMF_df <- read.csv(paste0(AMF_path,"AMF_cleaned_hourly_",arrayid,".csv"))
-# Calculate ET. convert unit from Wm-2 to mmday-1
-AMF_df$ET <- AMF_df$LE_F * 3600 *24/(2.45*10^6)
-AMF_df$ET[AMF_df$ET < 0] <- 0
-
-# Get the Site ID in this file
-Site_ID_ls <- unique(AMF_df$Site_ID)
-# Loop over each site and calculate its target variables
-progressr::with_progress({
-  p <- progressor(along = 1:length(Site_ID_ls))
+# There are 5 cleaned AMF files, arrayid determines which file to process
+for(arrayid in 1:5){
+  # Read in the cleaned AMF hourly data
+  AMF_df <- read.csv(paste0(AMF_path,"AMF_cleaned_hourly_",arrayid,".csv"))
+  # Calculate ET. convert unit from Wm-2 to mmday-1
+  AMF_df$ET <- AMF_df$LE_F * 3600 *24/(2.45*10^6)
+  AMF_df$ET[AMF_df$ET < 0] <- 0
   
-  future_lapply(1:length(Site_ID_ls),function(i){
-    siteid <- Site_ID_ls[i]
-    # Only keep data for this site
-    Site_df <- AMF_df %>%
-      filter(Site_ID == siteid)
-    # Calculate soil water potential based on soil moisture and soil texture at this site
-    # Reference: Lowman et al., 2023
-    Site_df$psi_soil <- Cal_psisoil(Site_df,site_info,siteid)
-    # Output this df
-    write.csv(Site_df,paste0(AMF_path,"AMF_sites_hourly/AMF_hourly_",siteid,".csv"))
-    p()
-  })
-})
+  # Get the Site ID in this file
+  Site_ID_ls <- unique(AMF_df$Site_ID)
+  # Only keep sites that exist in site_info
+  Site_ID_ls <- Site_ID_ls[Site_ID_ls %in% site_info$site_id]
+  
+  # Loop over each site and calculate its target variables
+  progressr::with_progress({
+    p <- progressor(along = 1:length(Site_ID_ls))
+    
+    future_lapply(1:length(Site_ID_ls),function(i){
+      siteid <- Site_ID_ls[i]
+      # Only keep data for this site
+      Site_df <- AMF_df %>%
+        filter(Site_ID == siteid)
+      # Calculate soil water potential based on soil moisture and soil texture at this site
+      # Reference: Lowman et al., 2023
+      Site_df$psi_soil <- Cal_psisoil(Site_df,site_info,siteid)
+      # Output this df
+      write.csv(Site_df,paste0(AMF_path,"AMF_sites_hourly_update/AMF_hourly_",siteid,".csv"))
+      p()
+    })
+  }) 
+  message(arrayid)
+}
+
 
 
 
