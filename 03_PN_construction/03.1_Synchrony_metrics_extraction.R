789e5722c3c43fa09df63310424c1b8ff0c76136
endlessczz <endlessczz@gmail.com>
2025-08-17 15:54:53 -0500

    Calculate syc metrics for all 12 pairs for the target time period, at one site

--- 03_PN_construction/03.1_Synchrony_metrics_extraction.R
+++ 03_PN_construction/03.1_Synchrony_metrics_extraction.R
@@ -2,21 +2,51 @@
 # Date: 2025.8.17
 
 # This code is to extract Synchrony metrics (peak TE, memory, lag)
-
+# Peak TE: maximum significant TE normalized by Shannon entropy of the sink
+# Memory: time required for TE to become insignificant
+# Lag: best lag corresponding to peak TE
+# Note: Focus on full TE, GS, and NGS for now. No years results included
 
 # ------ Global ------ 
 library(dplyr)
 
 # Input path for TE_df
 TE_df_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Results/Hourly_TE_all_sites_server/Results/TE_df/"
-# Input path for Var_plots
-Var_plots_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Results/Hourly_TE_all_sites_server/Results/Var_plots/"
 # Updated site info
 Site_info <- read.csv("00_Data/ameriflux_site_info_update_GS.csv")
+# Source functions
+source("03_PN_construction/Synchrony_metrics_functions.R")
+
+max_lag <- 72 # Maximum lag to consider
+# All variable pairs to consider
+var_ls <- c("ET","psi","VPD","TA")
 
 # ------- Main ------
-# Get a list of Site_ID with valid TE output
-Site_ls <- unique(substr(list.files(Var_plots_path),11,16))
+# Determines which Site to proceed
+arrayid <- 1
+Site_ID <- Site_info$site_id[arrayid]
+# All variable combinations
+var_comb <- expand.grid(from = var_ls,
+                        to = var_ls) %>%
+  filter(from != to)
 
-setdiff(Site_info$site_id,Site_ls)
+# For full year =======
+file_name <- paste0("TE_df_ls_full_TS_",Site_ID,".rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ 03_PN_construction/Synchrony_metrics_functions.R
@@ -0,0 +1,66 @@
+# Author: Zhaozhe Chen (zhaozhe.chen@wisc.edu)
+# These codes are to analyze synchrony metrics based on TE results
+
+# This function calculates synchrony metrics
+# Input include the TE_df
+# Output include:
+# p_TE: peak TE
+# p_lag: lag corresponding to peak TE
+# memory: time required for TE to drop below critical TE
+cal_syc_metrics <- function(TE_df){
+  # Normalize TE and TEcrit
+  TE_df_tmp <- TE_df %>%
+    mutate(TE_norm = TE/Hy * 100,
+           TEcrit_norm = TEcrit/Hy * 100) %>%
+    select(Lag,TE_norm,TEcrit_norm)
+  # Only consider the first 24 hours
+  TE_df_1day <- TE_df_tmp[1:24,]
+  # Only consider significant values
+  TE_df_1day <- TE_df_1day %>%
+    mutate(TE_norm = if_else(TE_norm < TEcrit_norm,
+                             NA,TE_norm))
+  # Get peak TE
+  p_TE <- max(TE_df_1day$TE_norm,na.rm=TRUE)
+  # Get corresponding lag
+  p_lag <- TE_df_1day$Lag[which(TE_df_1day$TE_norm == p_TE)]
+  # Get memory
+  memory <- which(TE_df_tmp$TE_norm < TE_df_tmp$TEcrit_norm)[1]
+  # If TE never drops below critical TE, assign max_lag to it
+  if(is.na(memory)){
+    memory <- max_lag
+  }
+  return(c(p_TE,p_lag,memory))
+}
+
+# This function calculates syc metrics for all pairs of variables
+# For the selected time period at this site
+# Input include:
+# file_name: This should be the name for the TE_df_ls
+# Output: a vector of length 36, for syc metrics for all pairs of variables
+cal_syc_metrics_all_pairs <- function(file_name){
+  # Read in TE_df_ls
+  TE_df_ls <- readRDS(paste0(TE_df_path,file_name))
+  # Loop over each variable pair
+  # Initialize a vector to store all output syc metrics for all pairs
+  syc_metrics_all <- c()
+  for(i in 1:nrow(var_comb)){
+    # Source and sink name
+    source_name <- as.character(var_comb$from[i])
+    sink_name <- as.character(var_comb$to[i])
+    # Get TE_df for this pair
+    TE_df_name <- paste0(source_name,"_to_",sink_name)
+    TE_df <- TE_df_ls[[TE_df_name]]
+    # Check if this TE_df is valid
+    if(nrow(TE_df) < max_lag){
+      # All NA
+      syc_metrics <- c(NA,NA,NA)
+    }else{
+      # Calculate Synchrony metrics if valid
+      syc_metrics <- cal_syc_metrics(TE_df)
+    }
+    # Name these metrics
+    names(syc_metrics) <- paste0(c("p_TE_","p_lag_","mem_"),TE_df_name)
+    syc_metrics_all <- c(syc_metrics_all,syc_metrics)
+  }
+  return(syc_metrics_all)
+}
