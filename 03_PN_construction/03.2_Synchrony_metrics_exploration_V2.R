540641ce954cf3fc1b9fd4fbec04f9eb675b9023
endlessczz <endlessczz@gmail.com>
2026-02-10 22:17:45 -0600

    Make TE vs lag plot

--- 03_PN_construction/03.2_Synchrony_metrics_exploration_V2.R
+++ 03_PN_construction/03.2_Synchrony_metrics_exploration_V2.R
@@ -1,5 +1,5 @@
 # Author: Zhaozhe Chen
-# Update Date: 2026.2.7
+# Update Date: 2026.2.10
 
 # This code is to explore synchrony strength among pairs
 
@@ -8,6 +8,7 @@ library(dplyr)
 library(tidyr)
 library(ggpubr)
 library(FSA) # For Dunn test
+library(forcats)
 
 # Input path for Synchrony metrics for 12 pairs
 Syc_metrics_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Results/Hourly_TE_all_sites_server/Results/Syc_metrics_12pairs/"
@@ -31,7 +32,7 @@ Output_path <- "D:/OneDrive - UW-Madison/Research/ET Synchrony/Results/Hourly_TE
 season <- "GS"
 
 # Determine which syc metric to process
-arrayid <- 1
+arrayid <- 3
 # Name of target synchrony metrics
 res_varname <- c("daily_p_TE","daily_agg_TE","best_lag")[arrayid]
 # Title of these metrics
@@ -66,7 +67,7 @@ sc_pairs <- data.frame(source = c("psi","VPD","TA"),
 g_map_ls <- list()
 # Initialize a df to store synchrony subset
 syc_df_tmp <- c()
-# Initiliaze a list to store scatter and box plots
+# Initialize a list to store scatter and box plots
 g_scatter_box_ls <- list()
 
 # Colors for scatter and box plots
@@ -128,7 +129,7 @@ for(i in 1:nrow(sc_pairs)){
   # Continuous color palette
   map_color <- wes_palette("Zissou1",100,type="continuous")
   g_map <- syc_map(syc_df_sub,res_name,colors = map_color,
-                   legend_title = res_title,g_title = "",color_limits = c(0,15),end_marks = "right")
+                   legend_title = res_title,g_title = "",color_limits = c(0,24),end_marks = NA)
   g_map_ls[[i]] <- g_map
 }
 # Combine 3 maps for three pairs
@@ -183,13 +184,6 @@ g_box_pair <- ggplot(data = syc_df_tmp_paired,aes(x=source_sink,y=value,fill=sou
 
 print_g(g_box_pair,paste0("Syc_box_",res_name),4,3)
 
-# Synchrony metrics vs AI ==================
-
-# Synchrony metrics across climate
-
-# Synchrony metrics across PFT
-
-
 # Distributions of synchrony metrics across pairs ================================
 # Box plots of synchrony metrics across different pairs 
 g_box <- ggplot(data = syc_df,aes(x=source_sink,y=.data[[res_name]],fill=source_sink))+
@@ -206,59 +200,36 @@ g_box <- ggplot(data = syc_df,aes(x=source_sink,y=.data[[res_name]],fill=source_
     ET_TA  = expression(ET %->% T[air])
   ))+
   theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))
-print_g(g_box,paste0("Box_",res_name),6,6)
+print_g(g_box,paste0("Box_",res_name),3,4)
+
+#plot_pdf(syc_df,res_name,"source_sink",pair_color,1,"test")
 
 
-if(FALSE){
-# Comparing target syc metric between two source-sink pairs =================
-# Response synchrony metric name
-res_varname <- "best_lag"
+# Make plot including both lag and peak TE =========================
+g_lag_TE <- ggplot(data=syc_df,aes(x = GS_best_lag,y=forcats::fct_rev(source_sink)))+
+  geom_point(aes(size=GS_daily_p_TE^4,color=source_sink))+
+  scale_color_manual(values = pair_color)+
+  my_theme+
+  labs(x = "Lag (h)",y="")+
+  scale_y_discrete(labels = c(
+    psi_ET = expression(psi %->% ET),
+    ET_psi = expression(ET %->% psi),
+    VPD_ET = expression(VPD %->% ET),
+    ET_VPD = expression(ET %->% VPD),
+    TA_ET  = expression(T[air] %->% ET),
+    ET_TA  = expression(ET %->% T[air])
+  ))+
+  scale_size_continuous(
+    name = "Peak TE (%)",
+    breaks = c(2, 4, 8, 16)^4,   # breaks in plotted (transformed) space
+    labels = c("2", "4", "8", "16"),
+    range = c(2, 10)
+  )+
+  theme(legend.position = "right")
 
-# Determines which source-sink pair to test
-arrayid <- 1
+print_g(g_lag_TE,paste0("Lag_TE_plot"),8,4)
 
-source_name1 <- source_sink_pairs$Source1[arrayid]
-sink_name1 <- source_sink_pairs$Sink1[arrayid]
-source_name2 <- source_sink_pairs$Source2[arrayid]
-sink_name2 <- source_sink_pairs$Sink2[arrayid]
 
-# Make merged syc metrics, only keep target response variable (e.g., daily_p_TE), and calculate delta_response
-pair_syc_df_merged <- merge_pair_syc_df(source_name1,sink_name1,source_name2,sink_name2,res_varname)
-
-# Get title for the entire plot
-g_title <- paste("Comparison of",res_varname,"between",
-                 source_name1,"→",sink_name1,"vs",
-                 source_name2,"→",sink_name2,"\n",
-                 "+ =",res_varname,"(",source_name1,",",sink_name1,") >",
-                 res_varname,"(",source_name2,",",sink_name2,")")
-
-# Convert delta_response to binary values
-pair_syc_df_merged_binary <- pair_syc_df_merged %>%
-  mutate(delta_response = if_else(delta_response > 0,"+","-")) %>%
-  # Remove NA
-  filter(!is.na(delta_response)) %>%
-  # Only keep growing season
-  filter(Season == season)
-
-# Make binary map
-map_GS <- syc_map_disc(pair_syc_df_merged_binary %>% filter(Season == "GS"),
-                       "delta_response","Delta",
-                       g_title = "Growing Season")
-
-# Check Sites with positive delta values (psi-> ET dominant than ET->psi)
-# Make a map of these negative points, add site ID labels
-g <- ggplot()+
-  geom_sf(data=CONUS,fill="grey",color="black",alpha=0.3)+
-  geom_point(data = pair_syc_df_merged_binary %>%
-               filter(delta_response == "+"),
-             aes(x=longitude,y=latitude),
-             size=5,shape = 21,fill=my_color[1],color="black")+
-  geom_text_repel(data = pair_syc_df_merged_binary %>%
-                    filter(delta_response == "+"),
-                  aes(x=longitude,y=latitude,label = site_ID))+
-  map_theme
-print_g(g,"Source-sink_pair-comparisons/daily_p_TE_psi_ET_greater_than_ET_psi",5,4)
-}
 
 
 
